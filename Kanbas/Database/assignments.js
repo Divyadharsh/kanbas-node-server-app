7f1b95746cfe4f09f7b9a19964c5dba27e837447
Divyadharsh <divyadharshini0410@gmail.com>
2024-08-07 19:24:22 -0400

    Adding assignment.js file

--- Kanbas/Database/assignments.js
+++ Kanbas/Database/assignments.js
@@ -2,10 +2,32 @@ export default [
     { "_id": "A101", "title": "Propulsion Assignment", "course": "RS101" },
     { "_id": "A102", "title": "Combustion Analysis", "course": "RS101" },
     { "_id": "A103", "title": "Nozzle Design Project", "course": "RS101" },
+
     { "_id": "A201", "title": "Aerodynamics Quiz", "course": "RS102" },
     { "_id": "A202", "title": "Flow Analysis", "course": "RS102" },
     { "_id": "A203", "title": "Heating Analysis", "course": "RS102" },
+
     { "_id": "A301", "title": "Structural Design Task", "course": "RS103" },
     { "_id": "A302", "title": "Orbital Calculations", "course": "RS103" },
-    { "_id": "A303", "title": "Systems Engineering Exam", "course": "RS103" }
+    { "_id": "A303", "title": "Systems Engineering Exam", "course": "RS103" },
+
+    { "_id": "A401", "title": "Organic Chemistry_1", "course": "RS104" },
+    { "_id": "A402", "title": "Organic Chemistry_2", "course": "RS104" },
+    { "_id": "A403", "title": "Organic Chemistry_3", "course": "RS104" },
+
+    { "_id": "A501", "title": "InOrganic Chemistry_1", "course": "RS105" },
+    { "_id": "A502", "title": "InOrganic Chemistry_2", "course": "RS105" },
+    { "_id": "A503", "title": "InOrganic Chemistry_3", "course": "RS105" },
+
+    { "_id": "A601", "title": "Physical Chemistry_1", "course": "RS106" },
+    { "_id": "A602", "title": "Physical Chemistry_2", "course": "RS106" },
+    { "_id": "A603", "title": "Physical Chemistry_3", "course": "RS106" },
+
+    { "_id": "A701", "title": "Ancient Languages_1", "course": "RS107" },
+    { "_id": "A702", "title": "Ancient Languages_2", "course": "RS107" },
+    { "_id": "A703", "title": "Ancient Languages_3", "course": "RS107" },
+
+    { "_id": "A801", "title": "Political_1", "course": "RS108" },
+    { "_id": "A802", "title": "Political_2", "course": "RS108" },
+    { "_id": "A803", "title": "Political_3", "course": "RS108" }
   ]
