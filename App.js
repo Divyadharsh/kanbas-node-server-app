76f5f5c51d436a71ebd125532a2f953ea32a3c36
Divyadharshini Muruganandham <119703211+Divyadharsh@users.noreply.github.com>
2024-08-07 21:01:35 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
-import CourseRoutes from './Kanbas/Courses/Routes.js';
+import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 const app = express();
