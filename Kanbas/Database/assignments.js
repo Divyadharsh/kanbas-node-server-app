42c254dc44171af0c32a4abaae52a2c45de05c2e
Divyadharshini Muruganandham <divyadharshini0410@gmail.com>
2024-08-12 17:17:41 -0400

    Committing .json files to a6

--- Kanbas/Database/assignments.js
+++ Kanbas/Database/assignments.js
@@ -1,33 +1,92 @@
 export default [
-    { "_id": "A101", "title": "Propulsion Assignment", "course": "RS101" },
-    { "_id": "A102", "title": "Combustion Analysis", "course": "RS101" },
-    { "_id": "A103", "title": "Nozzle Design Project", "course": "RS101" },
-
-    { "_id": "A201", "title": "Aerodynamics Quiz", "course": "RS102" },
-    { "_id": "A202", "title": "Flow Analysis", "course": "RS102" },
-    { "_id": "A203", "title": "Heating Analysis", "course": "RS102" },
-
-    { "_id": "A301", "title": "Structural Design Task", "course": "RS103" },
-    { "_id": "A302", "title": "Orbital Calculations", "course": "RS103" },
-    { "_id": "A303", "title": "Systems Engineering Exam", "course": "RS103" },
-
-    { "_id": "A401", "title": "Organic Chemistry_1", "course": "RS104" },
-    { "_id": "A402", "title": "Organic Chemistry_2", "course": "RS104" },
-    { "_id": "A403", "title": "Organic Chemistry_3", "course": "RS104" },
-
-    { "_id": "A501", "title": "InOrganic Chemistry_1", "course": "RS105" },
-    { "_id": "A502", "title": "InOrganic Chemistry_2", "course": "RS105" },
-    { "_id": "A503", "title": "InOrganic Chemistry_3", "course": "RS105" },
-
-    { "_id": "A601", "title": "Physical Chemistry_1", "course": "RS106" },
-    { "_id": "A602", "title": "Physical Chemistry_2", "course": "RS106" },
-    { "_id": "A603", "title": "Physical Chemistry_3", "course": "RS106" },
-
-    { "_id": "A701", "title": "Ancient Languages_1", "course": "RS107" },
-    { "_id": "A702", "title": "Ancient Languages_2", "course": "RS107" },
-    { "_id": "A703", "title": "Ancient Languages_3", "course": "RS107" },
-
-    { "_id": "A801", "title": "Political_1", "course": "RS108" },
-    { "_id": "A802", "title": "Political_2", "course": "RS108" },
-    { "_id": "A803", "title": "Political_3", "course": "RS108" }
-  ]
+  {
+      "_id": "A101",
+      "title": "Propulsion Assignment",
+      "course": "RS101",
+      "description": "This assignment is available online.",
+      "points": 100,
+      "due_date": "2024-12-12",
+      "available_from": "2024-12-01",
+      "available_until": "2024-12-12"
+  },
+  {
+      "_id": "A102",
+      "title": "Combustion Analysis",
+      "course": "RS101",
+      "description": "This is a grate assignment",
+      "points": 20,
+      "due_date": "2024-12-13",
+      "available_from": "2024-12-02",
+      "available_until": "2024-12-13"
+  },
+  {
+      "_id": "A103",
+      "title": "Nozzle Design Project",
+      "course": "RS101",
+      "description": "This assignment is good.",
+      "points": 30,
+      "due_date": "2024-12-14",
+      "available_from": "2024-12-03",
+      "available_until": "2024-12-14"
+  },
+  {
+      "_id": "A201",
+      "title": "Aerodynamics Quiz",
+      "course": "RS102",
+      "description": "This assignment is better.",
+      "points": 40,
+      "due_date": "2024-12-15",
+      "available_from": "2024-12-05",
+      "available_until": "2024-12-15"
+  },
+  {
+      "_id": "A202",
+      "title": "Flow Analysis",
+      "course": "RS102",
+      "description": "This assignment is greater",
+      "points": 50,
+      "due_date": "2024-12-16",
+      "available_from": "2024-12-06",
+      "available_until": "2024-12-16"
+  },
+  {
+      "_id": "A203",
+      "title": "Heating Analysis",
+      "course": "RS102",
+      "description": "This assignment is the best.",
+      "points": 60,
+      "due_date": "2024-12-17",
+      "available_from": "2024-12-07",
+      "available_until": "2024-12-17"
+  },
+  {
+      "_id": "A301",
+      "title": "Structural Design Task",
+      "course": "RS103",
+      "description": "This assignment is the greatest",
+      "points": 70,
+      "due_date": "2024-12-18",
+      "available_from": "2024-12-08",
+      "available_until": "2024-12-18"
+  },
+  {
+      "_id": "A302",
+      "title": "Orbital Calculations",
+      "course": "RS103",
+      "description": "This assignment is ok.",
+      "points": 80,
+      "due_date": "2024-12-19",
+      "available_from": "2024-12-09",
+      "available_until": "2024-12-19"
+  },
+  {
+      "_id": "A303",
+      "title": "Systems Engineering Exam",
+      "course": "RS103",
+      "description": "This assignment is fine.",
+      "points": 90,
+      "due_date": "2024-12-02",
+      "available_from": "2024-11-01",
+      "available_until": "2024-12-02"
+  }
+]
